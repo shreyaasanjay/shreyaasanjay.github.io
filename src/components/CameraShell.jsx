67e5850abd4303bbf34c238ea620ed8abbae16c1
shreyaa <shreyaa.sanjay@gmail.com>
2026-07-20 09:36:44 -0400

    add skeleton and update sections

--- src/components/CameraShell.jsx
+++ src/components/CameraShell.jsx
@@ -5,7 +5,7 @@ function CameraShell({ sections, currentSection, onSectionChange }) {
   return (
     <section className="camera" aria-label="Portfolio camera">
       <div className="camera__top">
-        <span className="camera__brand">YOUR NAME</span>
+        <span className="camera__brand">Shreyaa Sanjay</span>
         <button className="shutter" type="button" aria-label="Featured project">
           <span />
         </button>
